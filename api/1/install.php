34b5ab859bc18f028e9370bdd1c030d6372c92f1
Eddy <eddy@em-creations.co.uk>
2014-12-21 13:39:00 +0000

    Update: Install script SQL statements added.

--- api/1/install.php
+++ api/1/install.php
@@ -10,33 +10,75 @@ $tableExistence = ["category", "delivery_option", "item", "item_image", "order",
 foreach ($tableExistence as $tableName) { // For each required table
 	try { // Try to query it
 		$db_conn->query("SELECT 1 FROM `" . $tableName . "` LIMIT 1");
-	} catch (PDOException $pdoE) { // If there's an exception it doesn't exist
+	} catch (PDOException $pdoE) { // If there's an exception the table doesn't exist
 		print("TABLE NAME: \"" . $tableName . "\" doesn't exist.. creating..<br />");
 		
 		switch ($tableName) {
-			// TODO: Add relevant SQL to these cases
 			case "category":
-				$query = "";
+				$query = "CREATE TABLE `category` (
+							`id` tinyint(1) NOT NULL AUTO_INCREMENT,
+							`name` varchar(100) NOT NULL,
+							`parent_id` bigint(20) NOT NULL,
+							PRIMARY KEY (`id`)
+						  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
 				break;
 			
 			case "delivery_option":
-				$query = "";
+				$query = "CREATE TABLE `delivery_option` (
+							`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+							`name` varchar(100) NOT NULL,
+							`max_weight` double NOT NULL,
+							`eco_rating` int(11) NOT NULL,
+							`cost` double NOT NULL,
+							PRIMARY KEY (`id`),
+							UNIQUE KEY `id` (`id`)
+						  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
 				break;
 			
 			case "item":
-				$query = "";
+				$query = "CREATE TABLE `item` (
+							`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+							`cat` bigint(20) NOT NULL,
+							`name` varchar(100) NOT NULL,
+							`description` text NOT NULL,
+							`weight` double NOT NULL,
+							`featured` tinyint(4) NOT NULL,
+							PRIMARY KEY (`id`),
+							UNIQUE KEY `id` (`id`)
+						  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
 				break;
 			
 			case "item_image":
-				$query = "";
+				$query = "CREATE TABLE `item_image` (
+							`item_id` int(11) NOT NULL,
+							`image` varchar(100) NOT NULL,
+							`main` tinyint(4) NOT NULL
+						  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 				break;
 			
 			case "order":
-				$query = "";
+				$query = "CREATE TABLE `order` (
+							`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+							`title` enum('Mr','Mrs','Miss','Ms','Dr') NOT NULL,
+							`first_name` varchar(100) NOT NULL,
+							`last_name` varchar(100) NOT NULL,
+							`address_1` varchar(100) NOT NULL,
+							`address_2` varchar(100) NOT NULL,
+							`post_code` char(8) NOT NULL,
+							`delivery` bigint(20) NOT NULL,
+							`created` bigint(20) NOT NULL,
+							PRIMARY KEY (`id`),
+							UNIQUE KEY `id` (`id`)
+						  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
 				break;
 			
 			case "order_item":
-				$query = "";
+				$query = "CREATE TABLE `order_item` (
+							`order_id` bigint(20) NOT NULL,
+							`item_id` bigint(20) NOT NULL,
+							`quantity` int(11) NOT NULL,
+							PRIMARY KEY (`order_id`,`item_id`)
+						  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 				break;
 			
 			default:
@@ -50,3 +92,5 @@ foreach ($tableExistence as $tableName) { // For each required table
 		}
 	}
 }
+
+print("Finished!");
--- api/config.inc.php
+++ api/config.inc.php
@@ -3,11 +3,17 @@
  * Author: Edward McKnight (UP608985)
  */
 
+/*
+ * Only Edit the below constant definitions
+ */
 define("DB_TYPE", "mysql");
 define("DB_HOST", "localhost");
 define("DB_NAME", "ecocart");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
+/*
+ * DO NOT MODIFY ANYTHING BELOW
+ */
 
 switch (DB_TYPE) {
 	case "mysql":
@@ -18,10 +24,11 @@ switch (DB_TYPE) {
 		$pdoStr = "mysql:";
 }
 
-$pdoStr .= "host=" . DB_HOST . ";" . "dbname=" . DB_NAME;
+$pdoStr .= "host=" . DB_HOST . ";" . "dbname=" . DB_NAME; // Build the PDO string
 
 try { // Try to connect to the database
 	$db_conn = new PDO($pdoStr, DB_USER, DB_PASSWORD);
+	// TODO: Modify the line below before completion
 	$db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set the error mode to exception FOR DEV ONLY!
 } catch (PDOException $pdoE) { // If an exception was caught
 	print("DATABASE CONNECTION ERROR: " . $pdoE->getMessage()); // Output the exception's message
